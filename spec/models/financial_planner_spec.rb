d5d33968de61f96e7929ed6f827325133e30e0d1
yamato0211 <kumayama0211@gmail.com>
2023-11-17 15:42:32 +0900

    change: should to is_expected.to

--- spec/models/financial_planner_spec.rb
+++ spec/models/financial_planner_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe FinancialPlanner, type: :model do
     end
 
     describe "validations" do
-        it { should validate_presence_of :name }
-        it { should validate_presence_of :description }
-        it { should validate_presence_of :email }
-        it { should validate_presence_of :password }
-        it { should validate_length_of :name }
-        it { should validate_length_of :description }
-        it { should validate_length_of :qualification }
+        it { is_expected.to validate_presence_of(:name) }
+        it { is_expected.to validate_presence_of(:description) }
+        it { is_expected.to validate_presence_of(:email) }
+        it { is_expected.to validate_presence_of(:password) }
+        it { is_expected.to validate_length_of(:name) }
+        it { is_expected.to validate_length_of(:description) }
+        it { is_expected.to validate_length_of(:qualification) }
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe User, type: :model do
     end
 
     describe "validations" do
-        it { should validate_presence_of :name }
-        it { should validate_presence_of :description }
-        it { should validate_presence_of :email }
-        it { should validate_presence_of :password }
-        it { should validate_length_of :name }
-        it { should validate_length_of :description }
+        it { is_expected.to validate_presence_of(:name) }
+        it { is_expected.to validate_presence_of(:description) }
+        it { is_expected.to validate_presence_of(:email) }
+        it { is_expected.to validate_presence_of(:password) }
+        it { is_expected.to validate_length_of(:name) }
+        it { is_expected.to validate_length_of(:description) }
     end
 end
