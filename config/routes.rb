843d335dbb378e6fdae5d54710fa5bfd15d65193
yamato0211 <kumayama0211@gmail.com>
2023-11-17 13:49:52 +0900

    add: router.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
+  devise_for :financial_planners, controllers: {
+    :confirmations => 'financial_planners/confirmations',
+    :registrations => 'financial_planners/registrations',
+    :sessions => 'financial_planners/sessions',
+    :passwords => 'financial_planners/passwords'
+  }
+  devise_for :users, controllers: {
+    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+  }
+
+  resources :users, only: [:index]
+  resources :financial_planners, only: [:index]
+  resources :signup
+  # get '/sign_up', to: 'signup#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "top#index"
 end
