6b1ff73be62f0d6b4296aeff9664893c1d32efb1
yamato0211 <kumayama0211@gmail.com>
2023-11-24 15:56:30 +0900

    add: custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'appointments/new'
+  get 'time_slot/new'
   devise_for :financial_planners, controllers: {
     confirmations: 'financial_planners/confirmations',
     registrations: 'financial_planners/registrations',
@@ -12,9 +14,13 @@ Rails.application.routes.draw do
     passwords: 'users/passwords'
   }
 
-  resources :users, only: [:index]
-  resources :financial_planners, only: [:index]
+  resources :users
+  resources :financial_planners
   resources :signup
+  resource :time_slots
+
+  # カスタムルートを追加
+  delete 'time_slots/destroy_all', to: 'time_slots#destroy_all', as: 'destroy_all_time_slots'
 
   root 'top#index'
 end
