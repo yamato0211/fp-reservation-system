aea4cd1964928e5230c85cbd7995ae056b6ee129
yamato0211 <kumayama0211@gmail.com>
2023-11-24 15:56:22 +0900

    add: financial top page

--- app/controllers/financial_planners_controller.rb
+++ app/controllers/financial_planners_controller.rb
@@ -2,7 +2,5 @@ class FinancialPlannersController < ApplicationController
   before_action :authenticate_financial_planner!
 
   def index
-    puts Date.today
-    @time_slots = TimeSlot.all.where("date >= ?", Date.today).where("date < ?", Date.today >> 3).order(date: :desc)
   end
 end
