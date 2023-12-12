7c284f440c5ae9eabdd4a08243315776959d60e6
yamato0211 <kumayama0211@gmail.com>
2023-12-12 14:43:58 +0900

    fix: 起動時のエラー解消

---
+++ app/models/simple_calendar/user_week_calendar.rb
@@ -0,0 +1,13 @@
+module SimpleCalendar
+  class SimpleCalendar::UserWeekCalendar < SimpleCalendar::WeekCalendar
+    def to_partial_path
+      'simple_calendar/user_week_calendar'
+    end
+  
+    def date_range
+      beginning = start_date.beginning_of_week
+      ending    = start_date.end_of_week
+      (beginning..ending).to_a
+    end
+  end  
+end
